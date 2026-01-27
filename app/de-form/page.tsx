79f71cf47ba36a39bcde9d1793a7125ce5bc8a86
atharvdsingh <dsingh9450492600@gmail.com>
2026-01-27 22:15:17 +0530

    created page de-form

---
+++ app/de-form/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props {}
+
+function Page(props: Props) {
+    const {} = props
+
+    return (
+     <>
+     <h1 className='flex justify-center items-center ' >
+        this is page for department expert form
+     </h1>
+     </>   
+    )
+}
+
+export default Page
